5ad0231239803dad7104dd204ee5ced8a089c144
corinnar <corinna.ravilious@unep-wcmc.org>
2023-03-17 16:17:59 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,10 @@ version = '0.1.0'
 
 # -- General configuration
 
+latex_elements = {
+  'extraclassoptions': 'openany,oneside'
+}
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
