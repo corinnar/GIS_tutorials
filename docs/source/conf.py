4fb70ca05565cad1bccad580ca92ad9cabf6b8f2
corinnar <corinna.ravilious@unep-wcmc.org>
2023-03-21 10:32:10 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,10 +46,11 @@ latex_elements = {
     'papersize': 'a4paper', 
 
 # The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '11pt',
+    'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-    'preamble': r'''
+    'preamble': '\\addto\\captionsenglish{\\renewcommand{\\contentsname}{Table of contents}}',
+    r'''
         \usepackage{charter}
         \usepackage[sfdefault]{roboto}
         \usepackage{inconsolata}
